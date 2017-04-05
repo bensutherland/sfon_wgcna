0a50af690dce47f11837a1c7bc18c40a94b7c4c5
Ben Sutherland <bsuther7@bitbucket.org>
2017-04-05 15:20:08 -0400

    Input data from eXpress instead of htseq

--- 01_scripts/01_edgeR_normalization.R
+++ 01_scripts/01_edgeR_normalization.R
@@ -1,6 +1,6 @@
-## Normalization of count data from HT-seq output
+## Normalization of count data from eXpress output
 # This is the first step of the WGCNA repo, but the input data comes from:
-# https://github.com/bensutherland/SE-reads_assemble-to-counts.git
+# https://github.com/bensutherland/Simple_reads_to_counts.git
 
 #rm(list=ls())
 
@@ -9,12 +9,18 @@
 #biocLite("edgeR")
 require("edgeR")
 
+biocLite("locfit")
+require("locfit")
+
 # User guides are here:
 # browseVignettes("edgeR")
 # edgeRUsersGuide()
 
-# Set working directory
+# Set working directory (Wayne)
 setwd("~/Documents/bernatchez/01_Sfon_projects/04_Sfon_eQTL/sfon_wgcna")
+
+# Macpro
+setwd("Documents/sfon_wgcna/")
 
 #### 1. Import Data ####
 # Import interpretation file
@@ -22,31 +28,63 @@ interp <- as.data.frame(read.csv("00_archive/sfeq_interpretation_v1.csv"))
 head(interp)
 names(interp)
 
-# Collect file names
-files <- list.files(path="02_input_data/", pattern="*htseq_counts.txt")
+
+# Import counts file
+my.counts <- read.csv(file = "02_input_data/out.matrix.csv")
+
+
+# Match the counts and interpretation
+# fix my.counts colnames
+new.names <- colnames(my.counts)
+new.names <- gsub(new.names,pattern = ".eff.counts", replacement = "")
+colnames(my.counts) <- new.names
+names(my.counts)
+
+# fix interp file
+interp$file.name <- gsub(interp$file.name, pattern = "_trimmed.fastq.gz.bam_htseq_counts.trim.txt", replacement = "")
+# note that we still don't have Arctic Charr in interpretation
 
 # Import data
-my.counts <- readDGE(files, path="02_input_data/", header = F) # Note: is an arg. for DGEList genes to provide annot.
-str(my.counts)
-dim(my.counts) #total unique tags = 69440
-my.counts.bk <- my.counts # just in case
+dim(my.counts) #total unique tags = 69441
 
-# Clean up file names
-to.trim <- "_R1_trimmed.fastq.gz.bam_htseq_counts"
-rownames(my.counts[[1]]) <- gsub(x = rownames(my.counts[[1]]), pattern = to.trim, replacement = "")
-my.counts$samples$files <- gsub(x = my.counts$samples$files, pattern = to.trim, replacement = "")
-dimnames(my.counts$counts)[[2]] <- gsub(x = dimnames(my.counts$counts)[[2]], pattern = to.trim, replacement = "")
-str(my.counts) # much cleaner now
+# my.counts.bk <- my.counts # just in case
 
-# also need to clean it up in the interpretation file (TO IMPROVE)
-test <- gsub(interp$file.name, pattern = to.trim, replacement = "")
-test <- gsub(test, pattern = "trim.", replacement = "")
-head(test)
-head(my.counts$samples$files) # looks to be similar format
 
-interp$file.name <- test
-names(interp)
-head(interp$file.name)
+# Set up DGEList
+rownames(my.counts) <- my.counts[,1]
+head(my.counts)
+
+my.counts.round <- round(my.counts[,-1])
+str(my.counts.round)
+
+### Do not use the gene names as a column
+# my.counts.in <- as.data.frame(cbind(my.counts[,1], my.counts.round))
+# colnames(my.counts.in)[1] <- "transcript.id"
+
+
+# create DGElist
+my.counts <- DGEList(counts = my.counts.round)
+
+
+# OLD
+# # Clean up file names
+# to.trim <- "_R1_trimmed.fastq.gz.bam_htseq_counts"
+# rownames(my.counts[[1]]) <- gsub(x = rownames(my.counts[[1]]), pattern = to.trim, replacement = "")
+# my.counts$samples$files <- gsub(x = my.counts$samples$files, pattern = to.trim, replacement = "")
+# dimnames(my.counts$counts)[[2]] <- gsub(x = dimnames(my.counts$counts)[[2]], pattern = to.trim, replacement = "")
+# str(my.counts) # much cleaner now
+# 
+# # also need to clean it up in the interpretation file (TO IMPROVE)
+# test <- gsub(interp$file.name, pattern = to.trim, replacement = "")
+# test <- gsub(test, pattern = "trim.", replacement = "")
+# head(test)
+# head(my.counts$samples$files) # looks to be similar format
+# 
+# interp$file.name <- test
+# names(interp)
+# head(interp$file.name)
+# END OLD (TO BE DELETED)
+
 
 #### 2. Filter Data ####
 # Find an optimal cpm filt (edgeRuserguide suggests 5-10 reads mapping to transcript)
@@ -70,6 +108,8 @@ dim(my.counts)
 my.counts <- calcNormFactors(my.counts, method = c("TMM"))
 my.counts$samples
 plot(my.counts$samples$norm.factors ~ my.counts$samples$lib.size)
+
+#NOT WORKING AFTER CHANGED INPUT 2017-04-05
 my.counts$samples$files[my.counts$samples$norm.factors < 0.8] # find which files are outliers in terms of norm.factors
 # is there anything specific about these files?
 
@@ -102,16 +142,17 @@ save.image(file = "02_input_data/sfon_wgcna_01_output.RData") # save out existin
 #### 5. Visualize data ####
 #plot using sample IDs
 plotMDS(x = my.counts, cex= 0.8) # note that this is supposed to be run on whatever you wrote calcNormFact() to
-#plot using sex
-plotMDS(x = my.counts, cex= 0.8
-        , labels = interp$sex[match(my.counts$samples$files, interp$file.name)])
-#plot using maturity and sex
-plotMDS(x = my.counts, cex= 0.8
-        , labels = paste(
-          interp$sex[match(my.counts$samples$files, interp$file.name)]
-            , interp$poids.sachet.foie[match(my.counts$samples$files, interp$file.name)]
-          , sep = ""))
 
-# note, this is how matching works:
-interp$sex[match(my.counts$samples$files, interp$file.name)] # matches order 
-interp$sex #see not the same
+# #plot using sex
+# plotMDS(x = my.counts, cex= 0.8
+#         , labels = interp$sex[match(my.counts$samples$files, interp$file.name)])
+# #plot using maturity and sex
+# plotMDS(x = my.counts, cex= 0.8
+#         , labels = paste(
+#           interp$sex[match(my.counts$samples$files, interp$file.name)]
+#             , interp$poids.sachet.foie[match(my.counts$samples$files, interp$file.name)]
+#           , sep = ""))
+# 
+# # note, this is how matching works:
+# interp$sex[match(my.counts$samples$files, interp$file.name)] # matches order 
+# interp$sex #see not the same
