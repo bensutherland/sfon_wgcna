ae7c781f6f15ff980ea45c1563ed5defd59de0e8
bensutherland <bensutherland7@gmail.com>
2017-10-02 08:25:51 -0700

    Add further interpretation script to analyze results

---
+++ 01_scripts/04_WGCNA_added_interpretation.R
@@ -0,0 +1,12 @@
+# Result interpretation
+
+## Clean space
+#rm(list=ls())
+
+# setwd
+setwd("/media/ben/Ariel/wgcna_output_from_xavier_REF_FEM_SEC_MALE/sfon_wgcna_REF_female/")
+
+# Load final wgcna object of the previous analysis
+load(file = "02_input_data/female_sfon_wgcna_save_point_step10.RData")
+
+# get the geneInfo for male
