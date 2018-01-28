aa923d656722ea9bc2b5cf72aa2f1cb1246d1c69
bensutherland <bensutherland7@gmail.com>
2018-01-28 10:34:36 -0800

    Loop plotting of sig chr-mod pairs accord to chr posit of genes

--- 01_scripts/plot_genes_on_chr.R
+++ 01_scripts/plot_genes_on_chr.R
@@ -1,7 +1,9 @@
-# Plot genes on chromosomes
-# input: chromosome lengths, bed file, transcript names of interest
+# Plots gene segments on chromosomes
 
-# use: <sex>_single_transcript_per_gene.txt, has start, end and moduleColor, transcript and target contig
+# Inputs: 
+## A. chr lengths: chr_of_interest_genome_lengths.txt
+## B. txome to genome align file w modules: <sex>_single_transcript_per_gene.txt
+## C. transcript/chr names of interest: <sex>_fisher_test_chr_enrich.txt
 
 # rm(list=ls())
 
@@ -10,69 +12,126 @@ setwd("~/Documents/10_bernatchez/10_paralogs")
 
 # Choose sex of coexpression modules
 #sex <- "female"
-sex <- "male"
+#sex <- "male"
 
-### Input files
-# 
-in.filename <- paste(sex, "_single_transcript_per_gene.txt", sep = "")
-data.file <- read.delim2(file = in.filename, header = T, sep = "\t")
-
-head(data.file)
-
-# chr length file
+##### 1. Input files ####
+# A. chr length file
 chr.length <- read.delim2(file = "chr_of_interest_genome_lengths.txt", header = F, sep = "\t")
 head(chr.length)
 
+# B. txome to genome align file w/ modules
+in.filename <- paste(sex, "_single_transcript_per_gene.txt", sep = "")
+data.file <- read.delim2(file = in.filename, header = T, sep = "\t")
+head(data.file)
+#issue w/ first line as NA
 
-### HERE ###
-
-
-par(mfrow=c(1,1), mar = c(4,4,3,3), cex = 0.6)
-
-# use plot to draw empty graph
-plot(1, type="n", xlab="", ylab="", xlim=c(1, 10), ylim=c(0, 10), yaxt = "n")
-## note: replace xlim with the max length of chromosomes of interest
-axis(2, at=1:10, labels=paste("chr_", seq(1:10)), las = 1)
-
-
-# Plot multiple chromosomes this way:
-segments(x0 = 1, y0 = 10, x1 = 10, y1 = 10, col = "black", lwd = 1)
-
-# add genes this way
-chr.pos <- 10 # this would be the level that the chr is at on the graph
-start <- 2 # this would be the starting position of the gene
-stop <- 3 # this would be the ending position of the gene
-
-segments(x0 = start, y0 = chr.pos, x1 = stop, y1 = chr.pos, col = "blue", lwd = 5)
+# C. transcripts/chr of interest file
+in.filename <- paste(sex, "_fisher_test_chr_enrich.txt", sep = "")
+chr.mods <- read.delim(file = in.filename, sep = "\t", header = T)
+head(chr.mods)
 
 
-# Plot a couple examples..
+#### 2. Subset to the chr/mod of interest #### 
+pval.cutoff <- 0.01
 
-# e.g. lightsteelblue1, contig NC_027317.1 
-#or NC_027312.1	yellowgreen
-#or NC_027302.1	grey60
+print(c("You are analyzing dataset: ", sex, "with enrich pval cutoff of: ", pval.cutoff ), sep = "", quote = F)
+sig.chr.mods <- chr.mods[chr.mods$pval < pval.cutoff, ] # only show those enriched with p < 0.01
+sig.chr.mods
 
-#or NC_027302.1	yellowgreen
-# NC_027312.1	skyblue
-# NC_027324.1	turquoise
-# NC_027325.1	yellow
-# NC_027305.1	plum1
-# NC_027304.1	steelblue
+# Merge signif data w/ chr length
+sig.chr.mods.and.length <- merge(x = sig.chr.mods, y = chr.length, by.x = "chr", by.y = "V1")
+colnames(sig.chr.mods.and.length)[6] <- "length"  # issue make this more adaptive
+head(sig.chr.mods.and.length)
 
-## these ones:
-#chr	mod	pval	g.in.chr.in.mod	g.not.in.chr.in.mod
-# NC_027317.1	lightsteelblue1	1.41506678012974E-05	4	2
-# NC_027312.1	yellowgreen	0.0006309297	5	10
-# NC_027316.1	turquoise	0.0018427814	1	348
-# NC_027302.1	grey60	0.0021096624	4	9
-# NC_027305.1	low.corr	0.003513191	372	9371
-# NC_027302.1	yellowgreen	0.0037478207	4	11
-# NC_027312.1	skyblue	0.0042669006	4	10
-# NC_027324.1	turquoise	0.0050767741	16	333
-# NC_027325.1	yellow	0.0052164004	5	55
-# NC_027305.1	plum1	0.0061440002	4	14
-# NC_027304.1	steelblue	0.0073398353	4	15
-# NC_027305.1	grey	0.0087481258	348	6992
-# NC_027320.1	sienna3	0.009282496	3	18
+# Remove low.corr as these won't have modules
+sig.chr.mods.and.length <- sig.chr.mods.and.length[sig.chr.mods.and.length$mod != "low.corr", ]
 
-# choose those w/ p < 0.01, greater than 3 genes, and actual moduels (not low.corr or grey)
+# Remove un-enriched as these are not of interest (keep only those w/ at least 3 genes)
+sig.chr.mods.and.length <- sig.chr.mods.and.length[sig.chr.mods.and.length$g.in.chr.in.mod > 2, ]
+
+# Remove grey as this is not a true module
+sig.chr.mods.and.length <- sig.chr.mods.and.length[sig.chr.mods.and.length$mod != "grey", ]
+
+# sort it
+# # Make the data ordered so that it matches the y-axis
+sig.chr.mods.and.length <- sig.chr.mods.and.length[with(sig.chr.mods.and.length, order(sig.chr.mods.and.length$chr)), ]
+
+
+
+#### 3. Plot ####
+### Set up Plot ###
+# Find the necessary ylim for the plot
+max.ylim <- length(sig.chr.mods.and.length$chr) # based on the total unique chr involved
+
+# Find the necessary xlim for the plot
+max.xlim <- max(sig.chr.mods.and.length$length) # based on the longest chr
+
+# Draw empty plot
+par(mfrow=c(1,1), mar = c(4,6,3,3), cex = 0.6)
+
+# Plot empty graph
+plot(1, type="n", xlab="Position (bp)", ylab="", xlim=c(1, max.xlim), ylim=c(0, max.ylim), yaxt = "n")
+
+axis(2, at=1:max.ylim
+     , labels=sig.chr.mods.and.length$chr
+     #, labels=paste("chr_", seq(1:10))
+     , las = 1)
+
+### Loops to plot ###
+sig.chr.mods.and.length # this is our data to use
+
+# Pairs we want to plot
+plot.data <- unique(paste(sig.chr.mods.and.length$chr, sig.chr.mods.and.length$mod, sep = "-")) #join and uniq
+plot.data
+
+pairs.of.interest <- strsplit(plot.data, "-") # split it back up into constituents in a list
+pairs.of.interest 
+
+# Loop to plot each
+chr.of.interest <- NULL; mod.of.interest <- NULL
+sorted.sig <- NULL
+chr.pos <- NULL; chr.max <- NULL
+
+for(i in 1:length(pairs.of.interest)){
+  # print(pairs.of.interest[[i]])
+  chr.of.interest <- pairs.of.interest[[i]][1]
+  mod.of.interest <- pairs.of.interest[[i]][2]
+  print(chr.of.interest)
+  print(mod.of.interest)
+  
+  # Subset data to find the genes and their positions for the chr and mod of interest
+  data.of.interest <- subset(data.file, target.contig==chr.of.interest & moduleColor==mod.of.interest)
+  print(data.of.interest)
+  
+  # # Make the data ordered so that it matches the y-axis
+  # sorted.sig <- sig.chr.mods.and.length[with(sig.chr.mods.and.length, order(sig.chr.mods.and.length$chr)), ]
+  
+  # Find the max length of the chr of interest, and the y position for the y-axis
+  #chr.pos <- which(sig.chr.mods.and.length$chr == chr.of.interest) ## ISSUE HERE
+  chr.pos <- i
+  
+  chr.max <- sig.chr.mods.and.length[sig.chr.mods.and.length$chr == chr.of.interest, "length"][1]
+  # note that the [1] is used as chr are repeated in the file that is being queried
+  
+  # inner Loop to add segments
+  start <- NULL; stop <- NULL
+  chr.max
+  
+  for(g in 1:length(data.of.interest$transcript)){
+    start <- data.of.interest$start[g]
+    stop <- data.of.interest$end[g]
+    #print(c(start, stop))
+    segments(x0 = start, y0 = chr.pos, x1 = stop, y1 = chr.pos, col = mod.of.interest, lwd = 3)
+    points(x = start, y = chr.pos, pch="/")
+    points(x = stop, y = chr.pos, pch="|")
+    points(x = chr.max, y = chr.pos, pch=23)
+  }
+  
+  
+}
+
+
+# save out as
+# <sex>_chr_mod_enrichment.pdf in 5 x 3 inch
+
+
