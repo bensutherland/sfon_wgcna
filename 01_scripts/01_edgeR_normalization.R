f99b83439eb1ab67b18b07e22594e884354146cd
bensutherland <bensutherland7@gmail.com>
2018-02-24 14:43:36 -0800

    Remove old code, add header, add files reqd in edgeR_norm script

--- 01_scripts/01_edgeR_normalization.R
+++ 01_scripts/01_edgeR_normalization.R
@@ -2,6 +2,10 @@
 # This is the first step of the WGCNA repo, but the input data comes from:
 # https://github.com/bensutherland/Simple_reads_to_counts.git
 
+# Inputs:
+# 1) interpretation file
+# 2) counts file (linear)
+
 #rm(list=ls())
 
 ## Install Packages
@@ -16,20 +20,18 @@ require("locfit")
 # browseVignettes("edgeR")
 # edgeRUsersGuide()
 
-# Set working directory (Wayne)
-setwd("~/Documents/bernatchez/01_Sfon_projects/04_Sfon_eQTL/sfon_wgcna")
+# Set working directory
+#setwd("~/Documents/bernatchez/01_Sfon_projects/04_Sfon_eQTL/sfon_wgcna") #wayne
+setwd("~/Documents/10_bernatchez/01_sfon_eqtl/sfon_wgcna") #logan
 
-# Macpro
-setwd("Documents/sfon_wgcna/")
-
-#### 1. Import Data ####
+#### 1. Import Data, Set up Data ####
 # Import interpretation file
 interp <- as.data.frame(read.csv("00_archive/sfeq_interpretation_v1.csv"))
 head(interp)
 names(interp)
 
 
-# Import counts file
+# Import counts file (i.e. the final output from Simple_reads_to_counts repo)
 my.counts <- read.csv(file = "02_input_data/out.matrix.csv")
 
 
@@ -39,51 +41,24 @@ new.names <- colnames(my.counts)
 new.names <- gsub(new.names,pattern = ".eff.counts", replacement = "")
 colnames(my.counts) <- new.names
 names(my.counts)
-
-# fix interp file
-interp$file.name <- gsub(interp$file.name, pattern = "_trimmed.fastq.gz.bam_htseq_counts.trim.txt", replacement = "")
-# note that we still don't have Arctic Charr in interpretation
-
-# Import data
 dim(my.counts) #total unique tags = 69441
-
 # my.counts.bk <- my.counts # just in case
 
+# Fix filename in interp file
+interp$file.name <- gsub(interp$file.name, pattern = "_trimmed.fastq.gz.bam_htseq_counts.trim.txt", replacement = "")
+# note that we still don't have Arctic Charr in interpretation
 
 # Set up DGEList
 rownames(my.counts) <- my.counts[,1]
 head(my.counts)
 
+# Round all expression levels to no decimal place, remove transcript.id column (now rowname)
 my.counts.round <- round(my.counts[,-1])
 str(my.counts.round)
-
-### Do not use the gene names as a column
-# my.counts.in <- as.data.frame(cbind(my.counts[,1], my.counts.round))
-# colnames(my.counts.in)[1] <- "transcript.id"
 
 
 # create DGElist
 my.counts <- DGEList(counts = my.counts.round)
-
-
-# OLD
-# # Clean up file names
-# to.trim <- "_R1_trimmed.fastq.gz.bam_htseq_counts"
-# rownames(my.counts[[1]]) <- gsub(x = rownames(my.counts[[1]]), pattern = to.trim, replacement = "")
-# my.counts$samples$files <- gsub(x = my.counts$samples$files, pattern = to.trim, replacement = "")
-# dimnames(my.counts$counts)[[2]] <- gsub(x = dimnames(my.counts$counts)[[2]], pattern = to.trim, replacement = "")
-# str(my.counts) # much cleaner now
-# 
-# # also need to clean it up in the interpretation file (TO IMPROVE)
-# test <- gsub(interp$file.name, pattern = to.trim, replacement = "")
-# test <- gsub(test, pattern = "trim.", replacement = "")
-# head(test)
-# head(my.counts$samples$files) # looks to be similar format
-# 
-# interp$file.name <- test
-# names(interp)
-# head(interp$file.name)
-# END OLD (TO BE DELETED)
 
 
 #### 2. Filter Data ####
@@ -109,9 +84,9 @@ my.counts <- calcNormFactors(my.counts, method = c("TMM"))
 my.counts$samples
 plot(my.counts$samples$norm.factors ~ my.counts$samples$lib.size)
 
-#NOT WORKING AFTER CHANGED INPUT 2017-04-05
-my.counts$samples$files[my.counts$samples$norm.factors < 0.8] # find which files are outliers in terms of norm.factors
-# is there anything specific about these files?
+# #NOT WORKING AFTER CHANGED INPUT 2017-04-05
+# my.counts$samples$files[my.counts$samples$norm.factors < 0.8] # find which files are outliers in terms of norm.factors
+# # is there anything specific about these files?
 
 # Estimate dispersions (measure inter-library variation per tag)
 my.counts <- estimateDisp(my.counts) # note that this can use a design matrix when provided 
